2956684a7699d23b4776e731580447be86f0d6f4
Joshua Hoblitt <jhoblitt@cpan.org>
2009-02-17 19:37:19 +0000

    Pod::Tidy VERSION to '0.10'

--- lib/Pod/Tidy.pm
+++ lib/Pod/Tidy.pm
@@ -8,7 +8,7 @@ use strict;
 use warnings FATAL => qw( all );
 
 use vars qw( $VERSION $columns );
-$VERSION = '0.09';
+$VERSION = '0.10';
 
 use Fcntl ':flock';
 use File::Basename qw( basename dirname );
